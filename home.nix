af9b274bf096eb4e499a3547deddd5316f328226
Zoe Leullier <git@zoe.dev.br>
2025-12-28 13:22:27 -0300

    ssh: migrated to future home-manager syntax

--- home.nix
+++ home.nix
@@ -58,6 +58,21 @@ in
 
     programs.ssh = {
       enable = true;
+
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+
       extraConfig = ''
         Host *
             IdentityAgent "~/.1password/agent.sock"
